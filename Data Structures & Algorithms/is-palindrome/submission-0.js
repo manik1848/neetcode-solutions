f6c3ac5facf0afa10f000017dc6e1de41e308436
Manikandan P <100428609+manik1848@users.noreply.github.com>
2026-08-27 12:55:10 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isAlphaNumeric(c) {
+        return (c >= "A" && c <= "Z" || c >= "a" && c <= "z" || c >= "0" && c <= "9")
+    }
+    isPalindrome(s) {
+        let i = 0;
+        let j = s.length - 1;
+        while (i < j) {
+            if(!this.isAlphaNumeric(s[i])){
+                i++;
+                continue;
+            }
+            if(!this.isAlphaNumeric(s[j])){
+                j--;
+                continue;
+            }
+            if (this.isAlphaNumeric(s[i]) && this.isAlphaNumeric(s[j])) {
+                if (s[i].toLowerCase() !== s[j].toLowerCase()) {
+                    return false;
+                }
+                i++;
+                j--
+            }
+           
+        }
+        return true;
+    }
+}
