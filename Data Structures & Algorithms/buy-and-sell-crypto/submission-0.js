9e687a3cddc16c559c9a6e16adc7f8f5b43f457c
Manikandan P <100428609+manik1848@users.noreply.github.com>
2026-09-03 11:22:00 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let profit = 0;
+        let buy = prices[0];
+        for (let i = 1; i < prices.length; i++) {
+            profit = Math.max(profit, prices[i] - buy);
+            buy = Math.min(buy, prices[i]);
+        }
+        console.log(profit);
+        return profit;
+    }
+}
